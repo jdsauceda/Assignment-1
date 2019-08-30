7876778baf0ab5d8a20ff738e0c36eeb345e76cd
jdsauceda <mecajoe@yahoo.com>
2019-08-29 23:28:56 -0400

    Please work this time

---
+++ src/HellowWorld/HelloWorld.java
@@ -0,0 +1,10 @@
+package HellowWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello cruel world! This environment will take some time to get use to.");
+	}
+
+}
