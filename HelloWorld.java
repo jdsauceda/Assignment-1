9b9742cfaae8bc972a13f32b068437212b60e431
Joe Sauceda <mecajoe@yahoo.com>
2022-07-22 19:12:33 -0400

    update message

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello cruel world! This environment will take some time to get use to.");
+		System.out.println("Hello cruel world! This environment will take some time to get use to. I'm using this to commit something new.");
 	}
 
 }
